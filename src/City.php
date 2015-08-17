34e9690d2c775b50b1897e3609190abcdef490a5
Mukund Bhudia <mukund_bhudia@hotmail.com>
2015-08-17 22:05:00 +0100

    Changed city constructor to accept a string of values from the OWM text file. Added a test to ensure only 5 data values are accepted into the City object

--- src/City.php
+++ src/City.php
@@ -1,12 +1,19 @@
 <?php
 
 class City {
-    
-    public function __construct($cityID, $name, $latitude, $longitube, $countryCode) {
-        $this->cityID = $cityID;
-        $this->name = $name;
-        $this->$latitude = $latitude;
-        $this->$longitube = $longitube;
-        $this->$countryCode = $countryCode;
+    //Constructs City object taking in a string provided by OWM text file
+    public function __construct($cityString) {
+        //The string is tab separated so we explode over a tab
+        $cityValueArray = explode("\t" ,$cityString);
+        //OWM text data only has 5 vales per line
+        if (sizeof($cityValueArray) === 5) {
+            $this->cityID = $cityValueArray[0];
+            $this->name = $cityValueArray[1];
+            $this->latitude = $cityValueArray[2];
+            $this->longitude = $cityValueArray[3];
+            $this->countryCode = $cityValueArray[4];
+        } else {
+            throw new Exception("TooManyStringValues", 1);
+        }
     }
 }
--- test/CityTest.php
+++ test/CityTest.php
@@ -4,9 +4,22 @@ require_once "src/City.php";
 class CityTest extends PHPUnit_Framework_TestCase {
 
     public function testItCanGetCityName() {
-        $testCity = new City("819827", "Razvilka", "55.591667", "37.740833", "RU");
+        $testCity = new City("819827	Razvilka	55.591667	37.740833	RU");
 
         $this->assertEquals(819827, $testCity->cityID);
+        $this->assertEquals("Razvilka", $testCity->name);
+        $this->assertEquals(55.591667, $testCity->latitude);
+        $this->assertEquals(37.740833, $testCity->longitude);
+        $this->assertEquals("RU", $testCity->countryCode);
+    }
+
+    public function testItThrowsErrorForTooManyVariables() {
+        try {
+            $testCity = new City("819827	Razvilka	55.591667	37.740833	RU	1234");
+        } catch (Exception $e) {
+            $this->assertEquals('Exception', get_class($e));
+            $this->assertEquals('TooManyStringValues', $e->getMessage());
+        }
     }
 
 }
